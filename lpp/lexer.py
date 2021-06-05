a5aaf71f0a142b7f6aac380f2eb5c1a7f2debc81
NeoTRAN001 <neo_tran@outlook.com>
2021-06-05 01:48:33 -0500

    feat: create test to _make_two_character_token

--- lpp/lexer.py
+++ lpp/lexer.py
@@ -18,7 +18,10 @@ class Lexer:
         self._skip_whitespace()
 
         if match(r'^=$', self._character):
-            token = Token(TokenType.ASSIGN, self._character)
+            if self._peek_character() == '=':
+                token = self._make_two_character_token(TokenType.EQ)
+            else:
+                token = Token(TokenType.ASSIGN, self._character)
         elif match(r'^\+$', self._character):
             token = Token(TokenType.PLUS, self._character)
         elif match(r'^\-$', self._character):
@@ -46,7 +49,10 @@ class Lexer:
         elif match(r'^\>$', self._character):
             token = Token(TokenType.GT, self._character)
         elif match(r'^\!$', self._character):
-            token = Token(TokenType.NOT, self._character)
+            if self._peek_character() == '=':
+                token = self._make_two_character_token(TokenType.NOT_EQ)
+            else:
+                token = Token(TokenType.NOT, self._character)
         elif self._is_letter(self._character):
             literal = self._read_identifier()
             token_type = lookup_token_type(literal)
@@ -69,6 +75,19 @@ class Lexer:
 
     def _is_number(self, character: str) -> bool:
         return bool(match(r'^\d$', character))
+
+    def _make_two_character_token(self, token_type: TokenType) -> Token:
+        prefix = self._character
+        self._read_character()
+        suffix = self._character
+
+        return Token(token_type, f'{prefix}{suffix}')
+
+    def _peek_character(self) -> str:
+        if self._read_position >= len(self._source):
+            return ''
+        
+        return self._source[self._read_position]
 
     def _read_character(self) -> None:
         if self._read_position >= len(self._source):
