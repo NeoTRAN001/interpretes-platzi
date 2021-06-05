3b3a57a9b1ac00fbf22d0d5b5d8e363aa7ddf327
NeoTRAN001 <neo_tran@outlook.com>
2021-06-05 13:17:09 -0500

    feat: create parser

---
+++ test/parser_test.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from lpp.ast import (
+    LetStatement,
+    Program,
+)
+from lpp.lexer import Lexer
+from lpp.parser import Parser
+
+class ParserTest(TestCase):
+    def test_parse_program(self) -> None:
+        source: str = 'variable x = 5;'
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        self.assertIsNotNone(program)
+        self.assertIsInstance(program, Program)
+
+    def test_let_statements(self) -> None:
+        source: str = '''
+            variable x = 5;
+            variable y = 10;
+            variable foo = 20;
+        '''
+        lexer: Lexer = Lexer(source)
+        parser: Parser = Parser(lexer)
+
+        program: Program = parser.parse_program()
+
+        self.assertEqual(len(program.statements), 3)
+
+        for statement in program.statements:
+            self.assertEqual(statement.token_literal(), 'variable')
+            self.assertIsInstance(statement, LetStatement)
