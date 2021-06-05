0c3733a141c9118b6489d5842bac8169adfcda47
NeoTRAN001 <neo_tran@outlook.com>
2021-06-05 00:58:13 -0500

    feat: test to functions

--- test/lexer_test.py
+++ test/lexer_test.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from typing import List
-
 from lpp.token import (
     Token,
     TokenType
@@ -92,7 +91,60 @@ class LexerTest(TestCase):
         self.assertEquals(tokens, expected_tokens)
 
     def test_function_declaration(self) -> None:
-        pass
+        source: str = '''
+            variable suma = procedimiento(x, y) {
+                x + y;
+            };
+        '''
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(16):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'suma'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.FUNCTION, 'procedimiento'),
+            Token(TokenType.LPAREN, '('),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.RPAREN, ')'),
+            Token(TokenType.LBRACE, '{'),
+            Token(TokenType.IDENT, 'x'),
+            Token(TokenType.PLUS, '+'),
+            Token(TokenType.IDENT, 'y'),
+            Token(TokenType.SEMICOLON, ';'),
+            Token(TokenType.RBRACE, '}'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
     
     def test_function_call(self) -> None:
+        source: str = 'variable resultado = suma(dos, tres);';
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(10):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.LET, 'variable'),
+            Token(TokenType.IDENT, 'resultado'),
+            Token(TokenType.ASSIGN, '='),
+            Token(TokenType.IDENT, 'suma'),
+            Token(TokenType.LPAREN, '('),
+            Token(TokenType.IDENT, 'dos'),
+            Token(TokenType.COMMA, ','),
+            Token(TokenType.IDENT, 'tres'),
+            Token(TokenType.RPAREN, ')'),
+            Token(TokenType.SEMICOLON, ';'),
+        ]
+
+        self.assertEquals(tokens, expected_tokens)
+
+    def test_control_statement(self) -> None:
         pass
